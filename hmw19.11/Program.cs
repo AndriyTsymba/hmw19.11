1675000b08d4e0aafa64b915bd72f0bb879a0f2b
Kommandor <a.tsymbaliuk2007@gmail.com>
2024-12-21 14:24:37 +0200

    add task 2 hmw

--- hmw19.11/Program.cs
+++ hmw19.11/Program.cs
@@ -10,25 +10,14 @@ namespace hmw19._11
     {
         static void Main(string[] args)
         {
-
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            Console.WriteLine("Введіть довжину сторони квадрата:");
-            int line = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введіть символ для виведення:");
-            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введіть число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Виведення квадрата:");
-            for (int i = 0; i < line; i++)
-            {
-                for (int j = 0; j < line; j++)
-                {
-                    Console.Write(symbol);
-                }
-                Console.WriteLine();
-            }
+
+            Console.WriteLine(Palindrome.IsPalindrome(number));
         }
     }
 }
-
